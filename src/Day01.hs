08f98e640b1bea3289db248b39f47b3159109fd0
JackDyre <83248599+JackDyre@users.noreply.github.com>
2024-12-03 02:09:32 +0000

    🔒 2024 Day 01

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,16 +1,13 @@
-module Day01(run01, run01p1, run01p2) where
+module Day01 (run01) where
 
-import Control.Arrow((&&&))
-import Data.List (sort, transpose)
+import Control.Arrow ((&&&))
+import Control.Monad (liftM2)
+import Data.List (intersect, sort, transpose)
 
 run01 :: String -> (Int, Int)
-run01 = (run01p1 &&& run01p2) . parseInp01
+run01 = (runPt1 &&& runPt2) . parseInp
+  where
+    runPt1 = sum . map (abs . liftM2 (-) head last) . transpose . map sort
+    runPt2 = sum . liftM2 intersect last head
 
-parseInp01 :: String -> [[Int]]
-parseInp01 = transpose . map (map read . words) . lines
-
-run01p1 :: [[Int]] -> Int
-run01p1 = sum . map (\ x -> abs (head x - last x)) . transpose . map sort
-
-run01p2 :: [[Int]] -> Int
-run01p2 ns = sum $ map (\ n -> n * length (filter (n==) (last ns))) (head ns)
+    parseInp = transpose . map (map (read @Int) . words) . lines
